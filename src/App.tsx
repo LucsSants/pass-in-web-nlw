9116d289d71e61c4552cf955e7d5069a69dbd443
LucsSants <lucaslindo832@gmail.com>
2024-04-02 23:27:07 -0300

    class 01

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { AttendeeList } from "./Components/attendee-list";
+import { Header } from "./Components/header";
+
+export function App() {
+
+  return (
+      <div className="max-w-[1216px] mx-auto py-5 flex flex-col gap-5">
+        <Header/>
+        <AttendeeList/>
+      </div>
+    )
+}
+
+  
